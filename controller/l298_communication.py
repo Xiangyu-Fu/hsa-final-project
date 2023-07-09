590a91aea66929d1e3c038aa3b78196316e5e6fb
Lumen Yang <lumen.yang@lumeny.io>
2023-07-09 16:04:27 +0200

    communicate_with_l298n
    l298_communication_example
    
    successfully test the l298

---
+++ controller/l298_communication.py
@@ -0,0 +1,86 @@
+import RPi.GPIO as GPIO          
+from time import sleep
+
+in1_1 = 24
+in1_2 = 23
+en1 = 25
+temp1=1
+
+in2_1 = 17
+in2_2 = 27
+en2 = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1_1,GPIO.OUT)
+GPIO.setup(in1_2,GPIO.OUT)
+GPIO.setup(en1,GPIO.OUT)
+GPIO.output(in1_1,GPIO.LOW)
+GPIO.output(in1_2,GPIO.LOW)
+p=GPIO.PWM(en1,1000)
+p.start(50)
+print("\n")
+print("The default speed & direction of motor is LOW & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")    
+
+while(1):
+
+    x=input()
+    
+    if x=='r':
+        print("run")
+        if(temp1==1):
+         GPIO.output(in1,GPIO.HIGH)
+         GPIO.output(in2,GPIO.LOW)
+         print("forward")
+         x='z'
+        else:
+         GPIO.output(in1,GPIO.LOW)
+         GPIO.output(in2,GPIO.HIGH)
+         print("backward")
+         x='z'
+
+
+    elif x=='s':
+        print("stop")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        x='z'
+
+    elif x=='f':
+        print("forward")
+        GPIO.output(in1,GPIO.HIGH)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='z'
+
+    elif x=='b':
+        print("backward")
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=0
+        x='z'
+
+    elif x=='l':
+        print("low")
+        p.ChangeDutyCycle(25)
+        x='z'
+
+    elif x=='m':
+        print("medium")
+        p.ChangeDutyCycle(50)
+        x='z'
+
+    elif x=='h':
+        print("high")
+        p.ChangeDutyCycle(75)
+        x='z'
+     
+    
+    elif x=='e':
+        GPIO.cleanup()
+        break
+    
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
