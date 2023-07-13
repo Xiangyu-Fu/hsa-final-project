7f06bb5fd74c0fc90ba391ae61c8ac91b33746ff
Lumen Yang <lumen.yang@lumeny.io>
2023-07-13 20:05:41 +0200

    skeleton for real_robot.py

--- workspace/src/hsa_bot/scripts/real_robot.py
+++ workspace/src/hsa_bot/scripts/real_robot.py
@@ -5,3 +5,17 @@ This file contains the node of real robot to
 3) calculate the odometry of the robot
 4) broadcast the odometry of the robot
 """
+
+import rospy
+from geometry_msgs.msg import Twist
+
+
+def callback(data: Twist):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    return
+
+
+def cmd_vel_listener():
+    rospy.init_node('cmd_vel_listener', anonymous=True)
+    rospy.Subscriber("cmd_vel", Twist, callback)
+    rospy.spin()
