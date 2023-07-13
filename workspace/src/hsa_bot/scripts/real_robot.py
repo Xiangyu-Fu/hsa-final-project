14d68cab11afff88b50c072c48a68ffe81e009bc
Lumen Yang <lumen.yang@lumeny.io>
2023-07-13 14:57:58 +0200

    add real_robot.py

---
+++ workspace/src/hsa_bot/scripts/real_robot.py
@@ -0,0 +1,7 @@
+"""
+This file contains the node of real robot to 
+1) read from cmd_vel topic
+2) use the package diff_drive_controller to control the robot
+3) calculate the odometry of the robot
+4) broadcast the odometry of the robot
+"""
